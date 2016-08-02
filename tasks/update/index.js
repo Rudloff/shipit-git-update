ffb05edf83baff0217b7f7f47bc78732125dc6e4
Pierre Rudloff <contact@rudloff.pro>
2016-08-02 16:28:17 +0200

    Post-update command

--- tasks/update/index.js
+++ tasks/update/index.js
@@ -5,9 +5,11 @@ var utils = require('shipit-utils');
 module.exports = function (gruntOrShipit) {
     require('./checkout')(gruntOrShipit);
     require('./pull')(gruntOrShipit);
+    require('./post-update')(gruntOrShipit);
 
     utils.registerTask(gruntOrShipit, 'update', [
         'update:checkout',
-        'update:pull'
+        'update:pull',
+        'update:post-update'
     ]);
 };
---
+++ tasks/update/post-update.js
@@ -0,0 +1,17 @@
+/*jslint node: true*/
+'use strict';
+var utils = require('shipit-utils');
+var chalk = require('chalk');
+
+module.exports = function (gruntOrShipit) {
+    utils.registerTask(gruntOrShipit, 'update:post-update', function () {
+        var shipit = utils.getShipit(gruntOrShipit);
+
+        function checkout() {
+            shipit.log('Running post-update command');
+
+            return shipit.remote('cd ' + shipit.config.deployTo + ' && ' + shipit.config.postUpdateCmd);
+        }
+        return checkout();
+    });
+};
